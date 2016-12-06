2f14e199c5b1a1f5b379964b7c11a0974b943068
Viswajith V <viswajithiii@gmail.com>
2016-12-05 18:04:22 -0800

    Began web app

---
+++ analysis/analysis.py
@@ -0,0 +1,18 @@
+import sys
+sys.path.append('../')
+from nlp import utils as nlp_utils
+from utils import get_people_mentioned, get_sources
+
+
+def get_article_info(article_text):
+    article_text = str(article_text)
+    ann = nlp_utils.annotate_corenlp(article_text,
+                                     annotators=['pos', 'lemma', 'ner', 'parse',
+                                                 'depparse', 'parse', 'dcoref',
+                                                 'quote'])
+
+    sentences, corefs = ann['sentences'], ann['corefs']
+    people_mentioned = get_people_mentioned(sentences, corefs,
+                                            include_gender=True)
+    sources = get_sources(people_mentioned, sentences, corefs)
+    return people_mentioned, sources
