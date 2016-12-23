58a237c456cbc5044bfae48a38edc0ed998b0bec
Viswajith Venugopal <viswajithiii@gmail.com>
2016-12-22 16:25:15 -0800

    Shell script to automate annotation

---
+++ ann_NYT.sh
@@ -0,0 +1,17 @@
+year=$1
+startmonth=$2
+if [ "$3" == "D" ]
+then
+  path=/dfs/scratch0/viswa/gendermeme/NYT_temp/
+else
+  path=/lfs/madmax6/0/viswa/LDC2008T19_The-New-York-Times-Annotated-Corpus/data/data2/
+fi
+port=$4
+
+echo $year
+echo $startmonth
+echo $path
+for i in `seq $startmonth 4 12`; do
+  echo $i
+  python2.7 analysis/annotate_NYT.py $year $i  --port $port --path $path
+done
